54c355e483b8167c75ff86ff720dabf5560d2482
Fortune Emmanuel-king <56579818+4tunekvng@users.noreply.github.com>
2022-05-15 10:55:52 -0500

    assignment list created

--- lib/assignments.dart
+++ lib/assignments.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+final List<int> entries = <int>[1, 2, 3];
+
 class AssignmentPage extends StatefulWidget {
   const AssignmentPage({Key? key, required this.course}) : super(key: key);
 
@@ -18,6 +20,24 @@ class _MyAssignmentPageState extends State<AssignmentPage> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.course),
+      ),
+      body: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: entries.length,
+          itemBuilder: (BuildContext context, int index) {
+            return SizedBox(
+              height: 50,
+              child: Center(child: Text('Assignment ${entries[index]}')),
+            );
+          }
+
+      )
+
+    );
   }
 }
